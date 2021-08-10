b9581f93101d14b625de00611213782ef1da92d2
Hamza Hayd <dado.org@hotmail.com>
2021-08-10 15:32:59 +0200

    Formatted code

--- src/components/StartGame.vue
+++ src/components/StartGame.vue
@@ -1,72 +1,62 @@
 <template>
-    <div class="container">
+  <div class="container">
+    <nav class="nav">
+      <ul class="logo">
+        <li>
+          <a href="#"> TriviaApp</a>
+        </li>
+      </ul>
+      <ul class="navbar">
+        <li>
+          <a href="#">Start</a>
+        </li>
+        <li>
+          <a href="#">Questions</a>
+        </li>
+        <li>
+          <a href="#">Results</a>
+        </li>
+      </ul>
+    </nav>
 
-        <nav class="nav">
-            <ul class="logo">
-                <li>
-                    <a href="#"> TriviaApp</a>
-                </li>
-            </ul>
-            <ul class="navbar">
-                <li>
-                    <a href="#">Start</a>
-                </li>
-                <li>
-                    <a href="#">Questions</a>
-                </li>
-                <li>
-                    <a href="#">Results</a>
-                </li>
-            </ul>
-        </nav>
-
-        <div>
-
-            <form class="form-wrapper" action="#"> 
-
-<div class="difficulty">
-    
-              <label class="custom-select" for="styledSelect1">
-  <select id="styledSelect1" name="options">
-    <option value="">Select Difficulty</option>
-    <option value="1">Easy</option>
-    <option value="2">Medium</option>
-    <option value="3">Hard</option>
-
-  </select></label>
-</div>
-         <div class="category">
-               <label class="custom-select" for="styledSelect1">
-  <select id="styledSelect1" name="options">
-    <option value="">Select Category</option>
-    <option value="1">Easy</option>
-    <option value="2">Medium</option>
-    <option value="3">Hard</option>
-
-  </select></label>
-
-         </div>
-            
-
-          <div class="num-of-questions">
-
-            <label class="custom-select" for="styledSelect1">
-  <select id="styledSelect1" name="options">
-    <option value="">Select number of Questions</option>
-    <option value="1">Easy</option>
-    <option value="2">Medium</option>
-    <option value="3">Hard</option>
-
-  </select></label>
-          </div>
-
-
-               
-<button>Start Game</button>
-            </form>
+    <div>
+      <form class="form-wrapper" action="#">
+        <div class="difficulty">
+          <label class="custom-select" for="styledSelect1">
+            <select id="styledSelect1" name="options">
+              <option value="">Select Difficulty</option>
+              <option value="1">Easy</option>
+              <option value="2">Medium</option>
+              <option value="3">Hard</option>
+            </select></label
+          >
+        </div>
+        <div class="category">
+          <label class="custom-select" for="styledSelect1">
+            <select id="styledSelect1" name="options">
+              <option value="">Select Category</option>
+              <option value="1">Easy</option>
+              <option value="2">Medium</option>
+              <option value="3">Hard</option>
+            </select></label
+          >
         </div>
 
+        <div class="num-of-questions">
+          <label class="custom-select" for="styledSelect1">
+            <select id="styledSelect1" name="options">
+              <option value="">Select number of Questions</option>
+              <option value="1">Easy</option>
+              <option value="2">Medium</option>
+              <option value="3">Hard</option>
+            </select></label
+          >
+        </div>
+
+        <button>Start Game</button>
+      </form>
     </div>
+  </div>
 </template>
 
 <script>
@@ -85,7 +75,7 @@ export default {
             const resp = await fetch('https://opentdb.com/api.php?amount=10&category=10&difficulty=medium')
             const data = await resp.json()
             this.questions = data.results
-           
+
         }
     },
 
@@ -94,66 +84,61 @@ export default {
 }
 </script>
 <style scoped>
-    .container {
-      
-        padding: 4rem;
-    }
-    .nav {
-        display: flex;
-        justify-content: space-between;
-    }
-    .logo {
-        padding-left: 0;
-    }
-    .navbar {
-        width: 20vw;
-        display: flex;
-        justify-content: space-between;
-    }
-    .logo > li > a {
-        color: #A31621;
-        font-weight: bold;
-    }
-    .navbar > li > a {
-        color: #738290;
-        font-weight: bold;
-    }
-    .navbar > li > a:hover {
-        color: #f02d3a;
-    }
+.container {
+  padding: 4rem;
+}
+.nav {
+  display: flex;
+  justify-content: space-between;
+}
+.logo {
+  padding-left: 0;
+}
+.navbar {
+  width: 20vw;
+  display: flex;
+  justify-content: space-between;
+}
+.logo > li > a {
+  color: #a31621;
+  font-weight: bold;
+}
+.navbar > li > a {
+  color: #738290;
+  font-weight: bold;
+}
+.navbar > li > a:hover {
+  color: #f02d3a;
+}
 
+/*********Form ********** */
 
-
-    /*********Form ********** */
-
-   select {
-    padding: 12px;
-      width: 14rem;
-} 
+select {
+  padding: 12px;
+  width: 14rem;
+}
 
 .form-wrapper {
-    width: 60rem;
-    display: flex;
-    justify-content: space-around;
-    margin: 0 auto;
-    margin-top: 10rem;
+  width: 60rem;
+  display: flex;
+  justify-content: space-around;
+  margin: 0 auto;
+  margin-top: 10rem;
 }
-
 
 button {
-    cursor: pointer;
-    display: inline-block;
-    background:  #73EEDC;
-    color: #333;
-    border: 0;
-    padding: 12px;
-    width: 11rem;
+  cursor: pointer;
+  display: inline-block;
+  background: #73eedc;
+  color: #333;
+  border: 0;
+  padding: 12px;
+  width: 11rem;
 }
 button:focus {
-    outline: none;
+  outline: none;
 }
-button:hover{
-    transform: scale(0.98);
+button:hover {
+  transform: scale(0.98);
 }
- 
 </style>
