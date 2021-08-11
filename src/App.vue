f4352d1a6a776a255732965b203ae24da1c3d636
Hamza Hayd <dado.org@hotmail.com>
2021-08-11 14:21:21 +0200

    added nav component and cleaned up startGame

--- src/App.vue
+++ src/App.vue
@@ -3,32 +3,19 @@
     <Nav />
     <StartGame @review-submitted="addReview"> </StartGame>
 
-<<<<<<< HEAD
     <li v-for="query in queries" :key="query">{{ query }}</li>
   </div>
 </template>
-
 <script>
 import StartGame from "./components/StartGame.vue";
 import Nav from "./components/Nav.vue";
-//import DisplayQuestions from "./components/DisplayQuestions.vue";
-=======
-  <StartGame @review-submitted='addReview'>  </StartGame> 
-  
-  <li v-for="query in queries" :key="query"> {{query}} </li>
-</template>
-
-<script>
-
-import StartGame from './components/StartGame.vue'
->>>>>>> 591d658fb90d77110853de129f5e728480c3cd6e
 
 export default {
   name: "App",
   components: {
-<<<<<<< HEAD
-    StartGame,
     Nav,
+    StartGame,
+    // Form
   },
   data() {
     return {
@@ -46,27 +33,6 @@ export default {
 <style>
 body,
 html {
-=======
-
-    StartGame, 
-   // Form
-  },
-  data() {
-   return {
-     queries: []
-   }
-  },
-  methods: {
-    addReview(query) {
-      this.queries.push(query)
-    }
-  }
-} 
-</script>
-
-<style>
-body, html {
->>>>>>> 591d658fb90d77110853de129f5e728480c3cd6e
   margin: 0;
   padding: 0;
   box-sizing: border-box;
@@ -80,12 +46,7 @@ a {
 li {
   list-style-type: none;
 }
-<<<<<<< HEAD
 .container {
   padding: 4rem;
-=======
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif; 
->>>>>>> 591d658fb90d77110853de129f5e728480c3cd6e
 }
 </style>
