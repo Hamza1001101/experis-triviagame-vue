5f61c42475367d918e6ca7051d5352722058db9d
SebastianExperis <87641834+SebastianExperis@users.noreply.github.com>
2021-08-12 13:27:57 +0200

    Result

---
+++ src/components/Result.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "Result.vue",
+  data() {
+    return {
+      questions: [],
+      answers: [],
+      score: "",
+      home: "",
+      nextQ: "",
+    };
+  },
+
+
+}
+</script>
+
+<style scoped>
+
+</style>
